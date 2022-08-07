1c0c99bd72522cd9ecd21275ba038725f466c7b0
CacaAlves00 <cacazinho99@gmail.com>
2022-08-07 02:49:48 -0300

    fixing dragging (for mobiles)

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -73,14 +73,21 @@ function Card({ cardId, removeCard }) {
         const bounding = cardRef.current.getBoundingClientRect()
 
         setPosition((position) => {
-            return {
-                x: position.x + (e.clientX - bounding.x) - bounding.width / 2,
-                y: position.y + (e.clientY - bounding.y) - bounding.height / 2
-
-                // x: e.clientX,
-                // y: e.clientY
+            return isTouchDevice() ? {
+                x: position.x + (e.touches[0].clientX - bounding.x - bounding.width / 2),
+                y: position.y + (e.touches[0].clientY - bounding.y - bounding.height / 2)
             }
+                : {
+                    x: position.x + (e.clientX - bounding.x) - bounding.width / 2,
+                    y: position.y + (e.clientY - bounding.y) - bounding.height / 2
+                }
         })
+    }
+
+    function isTouchDevice() {
+        return (('ontouchstart' in window) ||
+            (navigator.maxTouchPoints > 0) ||
+            (navigator.msMaxTouchPoints > 0));
     }
 
     return (
